4fa59e706a6871ef0545be020f7400cb445fc42b
JulianaRaquel <julianamedeiros_228@hotmail.com>
2023-04-15 05:23:59 -0300

    incluido código para geração de certificados da plataforma

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import novo_evento, gerenciar_evento, inscrever_evento, participantes_evento, gerar_csv
+from .views import novo_evento, gerenciar_evento, inscrever_evento, participantes_evento, gerar_csv, certificados_evento, gerar_certificado
 
 
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('inscrever_evento/<int:id>', inscrever_evento, name='inscrever_evento'),
     path('participantes_evento/<int:id>', participantes_evento, name='participantes_evento'),
     path('gerar_csv/<int:id>', gerar_csv, name='gerar_csv'),
+    path('certificados_evento/<int:id>', certificados_evento, name="certificados_evento"),
+    path('gerar_certificado/<int:id>', gerar_certificado, name='gerar_certificado'),
 ]
